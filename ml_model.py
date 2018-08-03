e10c446b99f9aa33c181d5aa25bf80173500f0d1
Rishi Banerjee <rishieric91@gmail.com>
2018-08-03 18:53:22 +0700

    Added the deep learning libraries

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,15 @@
+# Importing data imputation and visualization libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+# Importing the Deep Learning Libraries for CNN from keras
+# Using tensorflow backend
+from keras.models import Sequential
+from keras.layers import (
+    Convolution2D, 
+    MaxPooling2D,
+    Flatten, 
+    Dense
+)   
