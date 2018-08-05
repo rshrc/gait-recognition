9cd40b9321744d06c4c7b108454dd3fb60e86a77
Kshitij Gupta <kshitijgm@gmail.com>
2018-08-05 22:58:54 +0530

    ml_model: Cleanup and add basic image loading
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- ml_model.py
+++ ml_model.py
@@ -2,20 +2,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
+import imageio as imio
+import glob
 # Importing the Deep Learning Libraries for CNN from keras
 
 # Using tensorflow backend
 from keras.models import Sequential
 from keras.layers import (
-    Convolution2D, 
+    Convolution2D,
     MaxPooling2D,
-    Flatten, 
+    Flatten,
     Dense
-)   
+)
 
 # We need to load the dataset, images
+image_list = []
+for im_path in glob.glob("imagesrc/001/bg-01/000/*.png"):
+    image = imio.imread(im_path)
+    image_list.append(image)
+
 
 # Initializing the CNN
 classifier = Sequential()
@@ -28,7 +33,7 @@ CNN - Convolutional Neural Network
 """
 We need to clarify about the input image size and decide on the activation function,
 For this, we have 32 feature detectors with 3x3 dimensions
-And should we use the relu function?? need to discuss 
+And should we use the relu function?? need to discuss
 """
 
 classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
@@ -37,9 +42,9 @@ classifier.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = '
 Pooling - Taken the definition from Wikipedia
 Convolutional networks may include local or global pooling layers
 which combine the outputs of neuron clusters at one layer into a single neuron in the next layer.
-For example, max pooling uses the maximum value from each of a cluster of neurons at the 
-prior layer. Another example is average pooling, which uses the average value from each of a 
-cluster of neurons at the prior layer. 
+For example, max pooling uses the maximum value from each of a cluster of neurons at the
+prior layer. Another example is average pooling, which uses the average value from each of a
+cluster of neurons at the prior layer.
 
 Do we require pooling though, given the image is already in black and white??
 """
@@ -49,18 +54,18 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 """
 Flattening the image
 you need to convert the output of the convolutional part of the CNN into a 1D feature vector,
-to be used by the ANN part of it. This operation is called flattening. It gets the output 
-of the convolutional layers, flattens all its structure to create a single long feature 
+to be used by the ANN part of it. This operation is called flattening. It gets the output
+of the convolutional layers, flattens all its structure to create a single long feature
 vector to be used by the dense layer for the final classification.
 """
 
 classifier.add(Flatten())
 
 """
-Full Connection :- 
-Fully connected layers connect every neuron in one layer to every neuron in another layer. 
-It is in principle the same as the traditional multi-layer perceptron neural network. 
-""" 
+Full Connection :-
+Fully connected layers connect every neuron in one layer to every neuron in another layer.
+It is in principle the same as the traditional multi-layer perceptron neural network.
+"""
 
 # First layer uses the relu activation function
 classifier.add(Dense(output_dim = 128, activation = 'relu'))
