c4b11a7c337caa847aeac41c73e8559b1169e5cf
rshrc <rishieric91@gmail.com>
2018-08-31 20:19:03 +0530

    Loading Model Data

---
+++ gait/model_data.py
@@ -0,0 +1,16 @@
+from keras.models import model_from_json
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
