fb40f9bd7f8c049f00951985b06a1261cd6592e4
rshrc <rishieric91@gmail.com>
2018-08-31 20:18:27 +0530

    Model Data Saving to .json
    Need to install h5py Python3 Module in the System

--- gait/main.py
+++ gait/main.py
@@ -132,3 +132,13 @@ classifier.fit_generator(
     nb_val_samples=2000
 )
 
+
+# Saving Model to File
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+
