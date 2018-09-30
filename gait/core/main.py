948fcfc1aca1d73ce348290c856934e412660486
Kshitij Gupta <kshitijgm@gmail.com>
2018-10-01 04:31:33 +0530

    wip: Functionalise and cleanup
    - Do everything via functions
    - Commonise things
    - Cleanup comments for now
    - Add functionality to read and write models
    - Update some Keras API calls
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- gait/core/main.py
+++ gait/core/main.py
@@ -1,140 +1,73 @@
-# Importing data imputation and visualization libraries
-import glob
-
-import imageio as imio
 from keras.layers import (
     Convolution2D,
     MaxPooling2D,
     Flatten,
     Dense
 )
-# Using tensorflow backend
-from keras.models import Sequential
-
-# Importing the Deep Learning Libraries for CNN from keras
-
-# We need to load the dataset, images
-imlist = []
-for path in sorted(glob.glob("../imagesrc/001/bg-01/*")):
-    imlist_cur = []
-    for im_path in sorted(glob.glob(path + "/*.png")):
-        image = imio.imread(im_path)
-        image = image / 255
-        imlist_cur.append(image)
-    imlist.append(imlist_cur)
-
-# Initializing the CNN
-classifier = Sequential()
-"""
-CNN's are used to analyze visual imagery,
-classify images and train them
-CNN - Convolutional Neural Network
-"""
-
-"""
-We need to clarify about the input image size and decide on the activation function,
-For this, we have 32 feature detectors with 3x3 dimensions
-And should we use the relu function?? need to discuss
-"""
-
-# Hyperparameters
-
-"""
-Features  = 4
-Feature Size = 3x3
-
-Image Size = 240x320
-Number of Color Channels = grayscale (1)
-"""
-
-classifier.add(Convolution2D(4, (3, 3), input_shape=(240, 320, 3), activation='relu'))
-
-"""
-Pooling - Taken the definition from Wikipedia
-Convolutional networks may include local or global pooling layers
-which combine the outputs of neuron clusters at one layer into a single neuron in the next layer.
-For example, max pooling uses the maximum value from each of a cluster of neurons at the
-prior layer. Another example is average pooling, which uses the average value from each of a
-cluster of neurons at the prior layer.
-
-Do we require pooling though, given the image is already in black and white??
-"""
-
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-"""
-Flattening the image
-you need to convert the output of the convolutional part of the CNN into a 1D feature vector,
-to be used by the ANN part of it. This operation is called flattening. It gets the output
-of the convolutional layers, flattens all its structure to create a single long feature
-vector to be used by the dense layer for the final classification.
-"""
-
-classifier.add(Flatten())
-
-"""
-Full Connection :-
-Fully connected layers connect every neuron in one layer to every neuron in another layer.
-It is in principle the same as the traditional multi-layer perceptron neural network.
-"""
-
-# First layer uses the relu activation function
-classifier.add(Dense(output_dim=560, activation='relu'))
-# If more than 2, we use the softmax activation function,
-# and the output neuron is using the softmax activation function
-classifier.add(Dense(output_dim=560, activation='relu'))
-classifier.add(Dense(output_dim=560, activation='relu'))
-classifier.add(Dense(output_dim=560, activation='relu'))
-classifier.add(Dense(output_dim=1, activation='sigmoid'))
-
-# Compiling the CNN
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-"""
-We need to start working from here, after we decide all the activation functions,
-and we decide how many layers and which optimization algotithm we have to use,
-and whether we need to flatten the images or not
-"""
-
-# Importing the liraries so as for fitting CNN to the Images
 from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.models import model_from_json
 
+def create_model():
+    model = Sequential()
+    model.add(Convolution2D(4, (3, 3), input_shape=(240, 320, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(output_dim=560, activation='relu'))
+    model.add(Dense(output_dim=560, activation='relu'))
+    model.add(Dense(output_dim=560, activation='relu'))
+    model.add(Dense(output_dim=560, activation='relu'))
+    model.add(Dense(output_dim=1, activation='sigmoid'))
+    return model
+
+def train(model, training_set, test_set):
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.fit_generator(
+            training_set,
+            steps_per_epoch=250,
+            epochs=25,
+            verbose=1,
+            validation_data=test_set,
+            validation_steps=62.5
+    )
+
+def save_model(model):
+    print("Saving...")
+    model.save_weights("model.h5")
+    print(" [*] Weights")
+    open("model.json", "w").write(model.to_json())
+    print(" [*] Model")
+
+def load_model():
+    print("Loading...")
+    json_file = open("model.json", "r")
+    model = model_from_json(json_file.read())
+    print(" [*] Model")
+    model.load_weights("model.h5")
+    print(" [*] Weights")
+    return model
+
+def dataset_provider(datagen):
+    return datagen.flow_from_directory(
+        'imagesrc',
+        target_size=(240, 320),
+        batch_size=32,
+        class_mode='binary'
+    )
+
+# Primary datagen
 train_datagen = ImageDataGenerator(
-    # rescale = 1./255, We dont have to rescale since we already have a black and white image
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True)
-
+# Validation datagen
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-# Training Set
-training_set = train_datagen.flow_from_directory(
-    'imagesrc/001',
-    target_size=(240, 320),
-    batch_size=32,
-    class_mode='binary', )
+# Primary Set for training
+training_set = dataset_provider(train_datagen)
+# Secondary / Test set for validation
+test_set = dataset_provider(test_datagen)
 
-test_set = test_datagen.flow_from_directory(
-    'imagesrc/001',
-    target_size=(240, 320),
-    batch_size=32,
-    class_mode='binary')
-
-classifier.fit_generator(
-    training_set,
-    samples_per_epoch=8000,
-    nb_epoch=25,
-    validation_data=test_set,
-    nb_val_samples=2000
-)
-
-model = classifier
-
-# Saving Model to File
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
+model = create_model()
+train(model, training_set, test_set)
+save_model(model)
