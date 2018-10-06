51cac59e1d59d5627a7075275db71e3c124a8dae
Caio Carvalho <caiojcarvalho@gmail.com>
2018-10-06 20:59:47 -0300

    Using cv2.VideoCapture and removing imutils dependency

--- live_detection.py
+++ live_detection.py
@@ -1,8 +1,5 @@
-from imutils.video import VideoStream
-from imutils.video import FPS
 import numpy as np
 import argparse
-import imutils
 import time
 import cv2
 
@@ -12,19 +9,8 @@ with open('yolov3.txt', 'r') as f:
 
 COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
 
-# ap = argparse.ArgumentParser()
-# ap.add_argument('-c', '--config', required=True,
-#                 help='path to yolo config file')
-# ap.add_argument('-w', '--weights', required=True,
-#                 help='path to yolo pre-trained weights')
-# ap.add_argument('-cl', '--classes', required=True,
-#                 help='path to text file containing class names')
-# args = ap.parse_args()
-
-
 def distance_to_camera(knownWidth, focalLength, perWidth):
     return (knownWidth * focalLength) / perWidth
-
 
 def get_output_layers(net):
     layer_names = net.getLayerNames()
@@ -37,34 +23,23 @@ def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
     cv2.putText(img, label+'('+str(int(confidence*100))+'%)', (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-
-
-
-
-    # net = cv2.dnn.readNet(args.weights, args.config)
-    # net = cv2.dnn.readNet('yolov3-tiny.weights', 'yolov3-tiny.cfg')
-    # net = cv2.dnn.readNet('yolov2.weights', 'yolov2.cfg')
 net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
-fps = FPS().start()
+vs = cv2.VideoCapture(0)
+start = time.time()
+
+frame_count = 0.0
 
 KNOWN_DISTANCE = 24.0
 KNOWN_WIDTH = 11.0
 
-image = cv2.imread("hello.jpeg")
-marker = find_marker(image)
-focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
-
-
 while True:
-        frame = vs.read()
-        frame = imutils.resize(frame, width=600)
+        ret, frame = vs.read()
+        cv2.resize(frame, (600, frame.shape[0]))
+
         scale = 0.00392
         (height, width) = frame.shape[:2]
         blob = cv2.dnn.blobFromImage(frame, scale, (416, 416), (0, 0, 0), True, crop=False)
-        # blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
         net.setInput(blob)
         outs = net.forward(get_output_layers(net))
         class_ids = []
@@ -99,15 +74,19 @@ while True:
             h = box[3]
             draw_prediction(frame, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
 
-       
+        frame_count += 1
+
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
         #if the `q` key was pressed, break from the loop
         if key == ord("q"):
              break
-        fps.update()
-        fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+end = time.time()
+
+time_elapsed = end - start
+
+print("[INFO] elapsed time: {:.2f}".format(time_elapsed))
+print("[INFO] approx. FPS: {:.2f}".format(frame_count / time_elapsed))
+vs.release()
 cv2.destroyAllWindows()
-vs.stop()
